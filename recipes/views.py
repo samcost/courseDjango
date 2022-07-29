da4b15f289e3843cb0e1f033eb9026fdc5eccfb9
Samantha Costa <samanthacosta@ufrn.edu.br>
2022-07-29 11:05:21 -0300

    initial structure

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
